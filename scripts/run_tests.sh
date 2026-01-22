b2a8b2d8b87ab50377cbe34a58c3e3589f65af82
Jason L. West <jlwestsr@gmail.com>
2026-01-22 08:23:56 -0600

    feat: integrate ansible, ansible-lint, and yamllint

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,5 +4,11 @@ set -e
 echo "Running Flake8..."
 flake8 src tests
 
+echo "Running Yamllint..."
+yamllint .
+
+echo "Running Ansible Lint..."
+ansible-lint ansible/
+
 echo "Running Pytest..."
 pytest
